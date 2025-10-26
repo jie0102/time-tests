70e39c120b388b52aa1b47760b47fe754aa005cf
Bowie <609568171@qq.com>
2025-10-26 18:29:55 +0000

    unit test

--- test_times.py
+++ test_times.py
@@ -7,4 +7,3 @@ def test_given_input():
     result = compute_overlap_time(large, short)
     expected = [('2010-01-12 10:30:00', '2010-01-12 10:37:00'), ('2010-01-12 10:38:00', '2010-01-12 10:45:00')]
     assert result == expected
-
